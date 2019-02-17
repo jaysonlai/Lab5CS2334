d4d589fd0ed12e4829ba37313e170ed3444683c0
Jayson <jayson.k.lai-1@ou.edu>
2019-02-17 16:18:01 -0600

    Beginning of lab 5

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 012
+Last Modified_______: 2019-02-17
+OU ID_______________: 113450461
+OU EMAIL____________: jayson.k.lai-1@ou.edu
+
+Tasks:
+1. Complete the protected static int methods in Calculator.java
+2. complete parseAndExecute method
+3. Complete calculateTwo AssertException methods
+4. Complete CalculateThree AssertException methods 
+5. Complete execute AssertException methods
+
+Estimated Time (minutes):
+1. 15
+2. 15
+3. 15
+4. 15
+5. 15
+
+Actual Time (minutes):
+1. 
+2. 
+3.
+4.
+5.
+*/}
